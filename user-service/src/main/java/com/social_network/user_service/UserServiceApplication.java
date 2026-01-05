57d94e7bffd5dce277c46d711f5e64620cb067ed
huuhuy1312 <flslayder1312@gmail.com>
2026-01-05 10:36:52 +0700

    Remove config server to new repo

--- user-service/src/main/java/com/social_network/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/social_network/user_service/UserServiceApplication.java
@@ -1,13 +1,24 @@
 package com.social_network.user_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class UserServiceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    @Autowired
+    private Environment env;
 
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String dataSourceUrl = env.getProperty("spring.datasource.url");
+        System.out.println("Datasource URL:" + dataSourceUrl);
+    }
 }
